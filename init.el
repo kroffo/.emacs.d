4241a3dea379bbab1a7a85ede668e585d1f767a1
Kenny <kenny@Kenny.attlocal.net>
2015-06-30 16:49:26 -0400

    Initialized repository and added git ignore.

---
+++ init.el
@@ -0,0 +1,7 @@
+;;; Kenny's emacs configuration file
+; Kenneth R Roffo Jr
+; June 23, 2015
+
+(setq-default indent-tabs-mode nil)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/base16-emacs")
+(load-theme 'base16-solarized-dark t)
