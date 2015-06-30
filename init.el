fbe19b932342f89ebbb4ef45c95483e62e41d764
Kenny <kenny@Kenny.attlocal.net>
2015-06-30 18:18:36 -0400

    Added column numbering, character limit of 80, and line highlighting.

--- init.el
+++ init.el
@@ -2,7 +2,23 @@
 ; Kenneth R Roffo Jr
 ; June 23, 2015
 
-(setq-default indent-tabs-mode nil)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/base16-emacs")
 (load-theme 'base16-solarized-dark t)
+
+;turn tabs into spaces
+(setq-default indent-tabs-mode nil)
+
+;turn on line numbering
 (global-linum-mode t)
+
+;display column number
+(column-number-mode t)
+
+;highlight the current line
+(global-hl-line-mode t)
+
+;set the line character limit
+(setq-default fill-column 80)
+
+;turn on auto-wrapping at the character limit
+(setq auto-fill-mode 1)
